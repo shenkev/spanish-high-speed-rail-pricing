553b1884285162f46d91d147025a03951f1670ff
shenkev <tingkekevin@hotmail.com>
2019-10-08 23:42:50 -0400

    final model

--- data.py
+++ data.py
@@ -10,9 +10,12 @@ from tqdm import tqdm
 
 tqdm.pandas()
 
-SAMPLE_RATE = 1.0
+SAMPLE_RATE = 0.01
+SAMPLES_TRAIN = 80000
+SAMPELS_VAL = 20000
 
 datetimeFormat = '%Y-%m-%d %H:%M:%S'
+
 def delta_min(a, b):
     diff = datetime.datetime.strptime(b, datetimeFormat) - datetime.datetime.strptime(a, datetimeFormat)
     return (diff.seconds/60.0)
@@ -24,23 +27,27 @@ def start_min(a, m):
 def main():
     data = pd.read_csv('./renfe.csv', index_col=0)
     data.shape
-    # print(data.head())
-    # print(data.info())
-    # print(data.isnull().sum())
 
-    data = data.sample(frac=SAMPLE_RATE, replace=False, random_state=1)
-    data.shape
+    # data = data.sample(frac=SAMPLE_RATE, replace=False, random_state=1)
     data = data[pd.notnull(data['price'])]  # seems to get rid of missing values in other fields as well
+    train_data, rest_data = data.iloc[:SAMPLES_TRAIN], data.iloc[SAMPLES_TRAIN:]
+    # need to make sure train_class and train_type are same with val data
+    for condition in ['train_class', 'train_type', 'origin', 'destination', 'fare']:
+        rest_data = rest_data[rest_data[condition].isin(train_data[condition].unique())]
+
+    data = pd.concat([train_data, rest_data.iloc[-SAMPELS_VAL:]])
+    data.shape
     # get one-hot features
     [origin, destination, train_type, train_class, fare] = [pd.get_dummies(data[x]) for x in ['origin', 'destination', 'train_type', 'train_class', 'fare']]
 
     min_date = data['start_date'].min()
 
     data['travel_time_in_min'] = data.progress_apply(lambda x:delta_min(x['start_date'], x['end_date']), axis=1) 
-    data['start_time_in_min'] = data.progress_apply(lambda x:delta_min(x['start_date'], min_date), axis=1) 
+    # data['start_time_in_min'] = data.progress_apply(lambda x:delta_min(x['start_date'], min_date), axis=1) 
 
     data.drop(['start_date','end_date'], axis=1,inplace=True)
-    new_data = data[['price', 'travel_time_in_min','start_time_in_min']]
+    # new_data = data[['price', 'travel_time_in_min','start_time_in_min']]
+    new_data = data[['price', 'travel_time_in_min']]
 
     for df in [origin, destination, train_type, train_class, fare]:
         new_data = pd.concat([new_data, df], axis=1)
@@ -52,12 +59,13 @@ def main():
 
     # save as numpy
     np_arr = new_data.values
-    np.random.shuffle(np_arr)
+    # np.random.shuffle(np_arr)
 
-    VAL_PERCENT = 0.20
-    VAL_INDEX = int(np_arr.shape[0]*VAL_PERCENT)
+    # VAL_PERCENT = 0.20
+    # VAL_INDEX = int(np_arr.shape[0]*(1.0-VAL_PERCENT))
+    VAL_INDEX = SAMPLES_TRAIN
 
-    val_X, val_y, train_X, train_y = np_arr[:VAL_INDEX, 1:], np_arr[:VAL_INDEX, 0], np_arr[VAL_INDEX:, 1:], np_arr[VAL_INDEX:, 0]
+    val_X, val_y, train_X, train_y = np_arr[VAL_INDEX:, 1:], np_arr[VAL_INDEX:, 0], np_arr[:VAL_INDEX, 1:], np_arr[:VAL_INDEX, 0]
 
     np.savez_compressed('./data/ready_data_{}.npz'.format(SAMPLE_RATE), train_X=train_X, train_y=train_y, val_X=val_X, val_y=val_y)
 
--- train.py
+++ train.py
@@ -8,13 +8,12 @@ DATA_FILE = "./data/ready_data_0.01.npz"
 loaded = np.load(DATA_FILE)
 [train_X, train_y, val_X, val_y] = [loaded[x] for x in ["train_X", "train_y", "val_X", "val_y"]]
 
-
 dtrain = xgb.DMatrix(train_X, label=train_y)
 dval = xgb.DMatrix(val_X, label=val_y)
 
 all_val_rmse = []
 all_val_rmse_map = {}
-
+              
 def hyper_search(experiments=10000):
 
     for _ in tqdm(range(experiments)):
@@ -23,23 +22,23 @@ def hyper_search(experiments=10000):
 def one_experiment():
 
     # params
-    num_round = random.randint(2,200)
+    num_round = random.randint(80, 80)
 
     param = {
     'objective':'reg:squarederror',
     'eval_metric':"rmse",
     'verbosity':0, 
     # 'booster': 'dart',
-    'eta': 0.1*(3.0**random.uniform(-4.0, 4.0)),
+    'eta': 0.1*(3.0**random.uniform(0.0, 0.0)),
     # 'gamma': 0,
-    'max_depth':random.randint(2,50),
-    'subsample': random.uniform(0.25, 1.0),
-    'colsample_bytree': random.uniform(0.25, 1.0),
-    # 'colsample_bynode': random.uniform(0.25, 1.0),
-    'lambda': 1.0*(2.0**random.uniform(-4.0, 4.0)),
-    'alpha': 0.0001*(10.0**random.uniform(0.0, 5.0)),
+    'max_depth':random.randint(25, 25),
+    'subsample': random.uniform(0.8, 0.8),
+    # 'colsample_bytree': random.uniform(1.0, 1.0),
+    'colsample_bynode': random.uniform(0.65, 0.65),
+    'lambda': 20.0*(5.0**random.uniform(0.0, 0.0)),
+    'alpha': 5.0*(5.0**random.uniform(0.0, 0.0)),
 
-    # 'rate_drop': 0.0,
+    # 'rate_drop': random.uniform(0.015, 0.05),
     # 'skip_drop': 0.0
     }
 
@@ -59,9 +58,16 @@ def one_experiment():
     if len(all_val_rmse) == 0 or val_perf < min(all_val_rmse):
         bst.save_model('./model/best.model')
 
+    # sorted_val = sorted(all_val_rmse)
+    # all_val_rmse_map[sorted_val[-1]]
+    # alpha = [all_val_rmse_map[sorted_val[i]][1]['alpha'] for i in range(len(sorted_val))]
+
     # save results
+    param['rounds'] = num_round
     all_val_rmse.append(val_perf)
     all_val_rmse_map[val_perf] = [bst.best_score, param]
+    print(val_perf)
+    print(bst.best_score)
 
 
 hyper_search()
